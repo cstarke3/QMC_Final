e465f89742b8286d62c72bd4e242beed9368e582
Bob Seaton <robert.w.seaton.jr@gmail.com>
2023-11-24 15:43:03 -0500

    getting closer

--- src/simple_model.py
+++ src/simple_model.py
@@ -29,7 +29,6 @@ class QMC(BaseModel):
         """ Initialize the simulation based on the input values. """
         np.random.seed(seed=self.seed)
 
-        # print(f"Initializing simulation with {self.n_part} particles.")
         xs_array = [0.0 for _ in range(self.n_part-1)]
 
         # sets the first min_replicas to be alive, the rest are dead
@@ -40,7 +39,6 @@ class QMC(BaseModel):
             
         # The initial value of the reference energy E_ref is the potential energy at the initial position of the replicas.
         self.Calculate_E_ref()
-        # print(f"Initial E_ref: {self.E_ref}")
     
     def replica_tot_pot(self, xs):
         """ Calculates the total potential energy of the system for a replica."""
@@ -79,14 +77,13 @@ class QMC(BaseModel):
         For each replica we calculate the potential energy between all particles in a replica:
             replicas[i].V_tot = V(x41) + V(x42) + V(x43) + V(x31) + V(x32) + V(x21) 
         and then take the average of each V_tot across the replicas
-            V_avg = (1/N_replicas ) sum
         """
         tot_pot_sum = 0.0
         
         if self.N == 0 or self.N is None:
             raise ValueError("No alive replicas found")
 
-        if self.E_ref is None: # accordinag to eqn 2.36, E_ref depends only on the number of alive replicas between steps
+        if self.E_ref is None: # according to eqn 2.36, after the first calc, E_ref depends only on the number of alive replicas between steps
             for replica in self.replicas[:self.N]:
                 xs_array = replica[1:]  # Exclude the first alive/dead element
                 tot_pot_sum += self.replica_tot_pot(xs_array)
@@ -111,29 +108,16 @@ class QMC(BaseModel):
         for ii,replica in enumerate(self.replicas):
             print(f"replica[{ii}]: {replica}")
                 
-    def clone_replica(self, replica):
-        """
-        Clones the replica into the slots of the first dead replica found.
-
-        Args:
-            replica (_type_): _description_
-        """
     def Branch(self):
         """ 
         This is the Birth/Death decision branch for each replica. 
         m_n = min[W(x), u_n]
         """
-        # prefactor = np.sqrt(self.delta_tau)
-        # W = 1 - ((self._V(self.replicas[i][0,k]) - self.Energy[-1]/self.hbar))*self._delta_t
-        # W = int(W + np.random.uniform())
-        # m = min(W, 3)
-        # NOTE: this is a guess, but we're trying to decide whether or not to kill a replica
-        # based on its potential energy relative to the reference energy
-        prefactor = self.delta_tau/hbar
+        dtau_over_hbar = self.delta_tau/hbar
         
         for replica in self.replicas[:self.N]:
             xs_array = replica[1:]  # Exclude the first alive/dead element
-            W = np.exp(-prefactor*(self.replica_tot_pot(xs_array) - self.E_ref))
+            W = np.exp(-dtau_over_hbar * (self.replica_tot_pot(xs_array) - self.E_ref)) # eqn 3.5
             m_n = min(int(W+np.random.uniform()), 3)
             if m_n == 0:  # Kill the replica
                 replica[0] = False
@@ -147,15 +131,9 @@ class QMC(BaseModel):
                         count_copies -= 1
 
 
-    def find_centroid(xs_array):
+    def centroid(xs_array):
         """
-        Find the centroid of the collectionn of particles based on the relative offsets contained in the xs_array
- 
-        Args:
-            xs_array (array/list): the relative offsets of the particles in the system referenced from the last particle
-
-        Returns:
-            _type_: _description_
+        Find the centroid of the particles based on the relative offsets contained in the xs_array
         """
         n = len(xs_array) + 1  # Total number of particles
         positions = [0.0] * n  # Initialize positions, assuming last particle is at the origin
